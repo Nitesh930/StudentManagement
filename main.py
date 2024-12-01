78b293cec9578a1320de62b52f70aeaa0d876ec3
unknown <singhniteshkumar1234@gmail.com>
2024-12-01 15:26:04 +0530

    Added root endpoint to main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routes.student_routes import router as student_router
 
 app = FastAPI(title="Student Management System")
-
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the FastAPI app!"}
 # Include student-related routes
 app.include_router(student_router, prefix="/api/v1/students")
